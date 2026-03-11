2282a8a9e1fd4de0fd412a81d2a82601f7ea719c
Néo CARDOT-BUTZ <neo.cardot-butz@etu.cyu.fr>
2026-03-11 23:13:41 +0100

    Refactor user data structure in client.json and update profil_admin.php for role management and user information display

--- html/profil_admin.php
+++ html/profil_admin.php
@@ -1,3 +1,5 @@
+<?php session_start();?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,33 +38,87 @@
                     <th>Bloquer</th>
                 </tr>
                 <tr>
-                    <td class="check" ><input type="checkbox" id="myCheckbox"><label for="myCheckbox"></label></td>
-                    <td><a href="profil_client.html">admin1</a></td>
-                    <td>#0000</td>
-                    <td><select name="role">
-                        <option value="role">Utilisateur</option>
-                        <option value="role">Livreur</option>
-                        <option value="role">Restaurateur</option>
-                        <option value="role">Administrateur</option>
-                    </select></td>
-                    <td>2025-01-01</td>
-                    <td><button class="bloquer">Bloquer</button></td>
+                    <form method="POST">
+                        <td class="check" ><input type="checkbox" id="myCheckbox"><label for="myCheckbox"></label></td>
+                        <td><input type="hidden" name="nom_utilisateur" value="neo@gmaildmin1"><a href="profil_client.html">neo@gmail</a></td>
+                        <td>#0000<input type="hidden" name="id_utilisateur" value="0000"></td>
+                        <td>   
+                            <select name="role">
+                                <option value="utilisateur">Utilisateur</option>
+                                <option value="livreur">Livreur</option>
+                                <option value="cuisinier">Cuisinier</option>
+                                <option value="administrateur">Administrateur</option>
+                            </select>
+                            <input type="submit">
+                        </td>
+                        <td>2025-01-01</td>
+                        </form>
+                        <td><form class="bloquer"><input type="submit"></form></td>
                 </tr>
                 <tr>
-                    <td class="check"><input type="checkbox" id="myCheckbox2"><label for="myCheckbox2"></label></td>
-                    <td><a href="profil_client.html">admin2</a></td>
-                    <td>#0001</td>
-                    <td><select name="role">
-                        <option value="role">Utilisateur</option>
-                        <option value="role">Livreur</option>
-                        <option value="role">Restaurateur</option>
-                        <option value="role">Administrateur</option>
-                    </select></td>
-                    <td>1s</td>
-                    <td><button class="bloquer">Bloquer</button></td>
+                    <form method="POST">
+                        <td class="check" ><input type="checkbox" id="myCheckbox"><label for="myCheckbox"></label></td>
+                        <td><input type="hidden" name="nom_utilisateur" value="admin2"><a href="profil_client.html">admin2</a></td>
+                        <td>#0001<input type="hidden" name="id_utilisateur" value="0001"></td>
+                        <td>   
+                            <select name="role">
+                                <option value="utilisateur">Utilisateur</option>
+                                <option value="livreur">Livreur</option>
+                                <option value="cuisinier">Cuisinier</option>
+                                <option value="administrateur">Administrateur</option>
+                            </select>
+                            <input type="submit">
+                        </td>
+                        <td>1s</td>
+                    </form>
+                    <td><form class="bloquer"><input type="submit"></form></td>
                 </tr>
             </table>
         </section>
     </div>
 </body>
 </html>
+
+<?php
+require_once __DIR__."/../serveur.php";
+
+/**
+ * Change le rôle d'un utilisateur et sauvegarde dans le fichier JSON.
+ * Vérifie également si l'utilisateur actuel a les droits admin.
+*/
+function changer_role(string $mail_utilisateur, string $nouveau_role) : bool{
+
+    if (empty($mail_utilisateur) or empty($nouveau_role)) return false;
+    if (!isset($_SESSION) or $_SESSION["role"] !== "administrateur") return false;
+    $data = lire_data("client.json");
+    if (!isset($data[$mail_utilisateur])) return false; // on retourne rien si l'utilisateur n'est pas trouvé
+
+    if (!isset($data[$mail_utilisateur]["parametre"]["modifiable"]) and $data[$mail_utilisateur]["parametre"]["modifiable"] == false) return false; // si le profil n'est pas modifiable (profil de secours) on ne modifie rien
+    $data[$mail_utilisateur]["role"] = $nouveau_role; // on change le role de l'utilisateur
+
+    $nouvelle_data = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents("client.json",$nouvelle_data);
+    return true;
+}
+
+function afficher_info(string $mail_utilisateur) : void{
+    $data = lire_data("client.json");
+    $utilisateur = $data[$mail_utilisateur];
+    if ($utilisateur == null) return;
+    echo "donnée lu <br>";
+    echo "nom utilisateur : ".$utilisateur["nom"]."<br>";
+    //echo "id utilisateur :".$utilisateur["id"]"<br>"; // n'existe pas
+    echo "role : ".$utilisateur["role"]."<br>";
+}
+
+afficher_info("neo@gmail.com");
+
+// test on utilisera la session déclarer lors de la connexion
+$_SESSION["role"] = "administrateur";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" and isset($_POST["role"])){
+    $nv_role = $_POST["role"];
+    changer_role("neo@gmail.com",$nv_role);
+}
+
+?>
--- serveur.php
+++ serveur.php
@@ -38,31 +38,72 @@ function add_new_cust(array $data, string $username, string $password): bool
 }
 */
 
-if ($user['password'] == $password){
-    switch ($user['role']) {
-    case 'cust':
-        header("Location: /html/profil_client.html");
+function lire_data(string $chemin, string $nom_utilisateur = "") : array{
+    if (!file_exists($chemin)) return [];
+    $data = json_decode(file_get_contents($chemin),true);
+    if ($data == null) return [];
+    if ($nom_utilisateur != ""){
+        if (isset($data[$nom_utilisateur])) return $data[$nom_utilisateur];
+    }
+    return $data;
+}
+/*
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if ($user['password'] == $password){
+        switch ($user['role']) {
+            case 'cust':
+                header("Location: /html/profil_client.php");
+                exit;
+                break;
+            case 'seller' :
+                header("Location: /html/commandes.php");
+                exit;
+                break;
+            case 'delivery' :
+                header("Location: /html/livraison.php");
+                exit;
+                break;
+            default:
+                header("Location: /html/connexion.php");
+                exit;
+                break;
+        }
+    }
+
+    else{
+        header("Location: /html/connexion.php");
         exit;
-        break;
-    case 'seller' :
-        header("Location: /html/commandes.html");
-        exit;
-        break;
-    case 'delivery' :
-        header("Location: /html/livraison.html");
-        exit;
-        break;
-    default:
-        header("Location: /html/connexion.html");
-        exit;
-        break;
     }
 }
 
-else{
-    header("Location: /html/connexion");
-    exit;
+format de la bdd :
+{
+    adresse mail{
+        "id" : ..., (id unique important !!)
+        "nom" : ...,
+        "prenom" : ...,
+        "mot de passe (hashé !)" : ...,
+        "contact" : {
+            "adresse" : ...,
+            "complement adress" : ...,
+            "telephone" : ...,
+            "adress mail" : ...,
+        },
+        "role" : ...,
+        "parametre" : {
+            "taille_police" : "12px",
+            "couleur" : "defaut",
+            "langue" : "fr"
+        },
+        "derniers-plats" : {...},
+        "securite" : {
+            "date_creation" : ...,
+            "derniere_connexion" : ...,
+            "est_banni" : false,
+            "est_en_ligne" : ... (timestemp),
+            "tentative_echec" : 0 (max 5)
+        }
+    }
 }
-
-
+*/
 ?>
